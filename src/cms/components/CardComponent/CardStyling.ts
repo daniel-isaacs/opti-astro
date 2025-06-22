d213ba489cef86571ceba6366d177baa9c4bf28d
Dan Isaacs <daniel.isaacs@optimizely.com>
2025-06-21 22:28:59 -0600

    WIP: Card styling/display updates

--- src/cms/components/CardComponent/CardStyling.ts
+++ src/cms/components/CardComponent/CardStyling.ts
@@ -9,6 +9,52 @@ export function getCardStyles(displaySettings: DisplaySettingsFragment[]): {
     let cssClasses: string[] = [];
 
     // Note: button styles managed via: src\cms\components\ButtonComponent\ButtonStyling.ts
+    return cssClasses;
+}
 
+export function getCardTextAlignmentStyle(
+    displaySettings: DisplaySettingsFragment[]
+): string[] {
+    const settings: Record<string, string> =
+        getDictionaryFromDisplaySettings(displaySettings);
+    let cssClasses: string[] = [];
+    switch (settings['textAlign']) {
+        case 'left':
+            cssClasses.push('text-left mr-auto');
+            break;
+        case 'center':
+            cssClasses.push('text-center mx-auto');
+            break;
+        case 'right':
+            cssClasses.push('text-right ml-auto');
+            break;
+        case 'justify':
+            cssClasses.push('text-justify');
+            break;
+        default:
+            break;
+    }
+    
+    return cssClasses;
+}
+
+
+export function getCardHeaderStyles(
+    displaySettings: DisplaySettingsFragment[]
+): string[] {
+    const settings: Record<string, string> =
+        getDictionaryFromDisplaySettings(displaySettings);
+    let cssClasses: string[] = [];
+    switch (settings['transformHeader']) {
+        case 'uppercase':
+            cssClasses.push('uppercase');
+            break;
+        case 'lowercase':
+            cssClasses.push('lowercase');
+            break;
+        case 'capitalize':
+            cssClasses.push('capitalize');
+            break;
+    }
     return cssClasses;
 }
