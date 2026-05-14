47f5da0804f1e79bdc0e5f2ba007580d274c97fd
Dan Isaacs <daniel.isaacs@optimizely.com>
2026-05-14 16:05:29 -0600

    Card display options

--- src/cms/components/CardComponent/CardHelper.ts
+++ src/cms/components/CardComponent/CardHelper.ts
@@ -2,10 +2,12 @@ export function getAssetWidthClass(dictionary: Record<string, string>): string {
     const widthValue = dictionary['assetWidth'];
     switch (widthValue) {
         case 'flex_1': return 'flex-1';
+        case 'w_1_4': return 'w-1/4';
         case 'w_1_3': return 'w-1/3';
         case 'w_2_5': return 'w-2/5';
         case 'w_1_2': return 'w-1/2';
         case 'w_3_5': return 'w-3/5';
+        case 'w_3_4': return 'w-3/4';
         case 'w_2_3': return 'w-2/3';
         default: return 'flex-1';
     }
@@ -15,10 +17,12 @@ export function getContentWidthClass(dictionary: Record<string, string>): string
     const widthValue = dictionary['contentWidth'];
     switch (widthValue) {
         case 'flex_1': return 'flex-1';
+        case 'w_1_4': return 'w-1/4';
         case 'w_1_3': return 'w-1/3';
         case 'w_2_5': return 'w-2/5';
         case 'w_1_2': return 'w-1/2';
         case 'w_3_5': return 'w-3/5';
+        case 'w_3_4': return 'w-3/4';
         case 'w_2_3': return 'w-2/3';
         default: return 'flex-1';
     }
